e50b7a4495f972c64cbaa993ab48705cdc439d40
pepuhodaniel <69733783+pepuhodaniel@users.noreply.github.com>
2020-11-19 21:15:58 +0900

    userdefined

--- learn/userdefined.sh
+++ learn/userdefined.sh
@@ -13,5 +13,5 @@ echo "Adik : $siapa,lagi ke $dimana,$siapa pergi tidak izin-izin";
 echo "Mama : aduhhh!! anak itu dia buat apa coba, terus dia pergi dari kapan?";
 echo "Adik : $siapa  pergi $mengapa dari tadi $kapan dengan teman-teman-nya";
 echo "Mama : ohhhh, tidak masalah kalau begitu!!";
-echo "Adik : kenapa saya tidak ikut ke $dimana.Sial"
+echo "Adik : kenapa saya tidak ikut ke $dimana.Sial";
 echo "tamat";
