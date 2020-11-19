b47c49702258ffeeed95211b8c3dd464d048644c
pepuhodaniel <69733783+pepuhodaniel@users.noreply.github.com>
2020-11-19 22:17:39 +0900

    Delete env_var.sh

--- learn/env_var.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Date is    :  $date";
-echo "Host Now is : $HOSTNAME";
-echo "User Now is : $USER";
-echo "Home Dir is : $HOME";
-echo "Model  	  : $DESKTOP_SESSION";
-echo "Logname     : $LOGNAME";
